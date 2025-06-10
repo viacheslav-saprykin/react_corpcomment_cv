575ada735f385f9cbb5d41519a47e0c9c59f26c9
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-06-10 20:29:36 +0300

    add: GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/react_corpcomment_cv/",
+  plugins: [react()]
+});
